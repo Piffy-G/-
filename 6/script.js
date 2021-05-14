3b30ff01ec046aa2017f14237ad2c29d5132bd75
Ilja <iljalozjuk@gmail.com>
2021-05-14 19:22:19 +0300

    first commit

---
+++ 6/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "5";
+
+const bool = false;
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Jhon",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+//              0           1        2      3        4  
+let arr = ['plum.png', 'orange.png', 6, 'apple.bmp', {}];
+console.log(arr[1]);
